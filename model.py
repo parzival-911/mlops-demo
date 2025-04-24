c0b607bd625b0a43ad7c5fa9a8ce0b00b5d5ca92
Rakshit <rakshittiwari643@gmail.com>
2025-04-24 19:34:29 +0530

    Initial commit - ML model script

---
+++ model.py
@@ -0,0 +1,15 @@
+#model.py
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+# Training data (simple numbers)
+X = np.array([[1], [2], [3], [4]])
+y = np.array([2, 4, 6, 8])
+
+# Train a linear regression model
+model = LinearRegression()
+model.fit(X, y)
+
+# Predict the value for input 5
+predicted = model.predict([[5]])
+print("Predicted number for 5 is:", predicted[0])
